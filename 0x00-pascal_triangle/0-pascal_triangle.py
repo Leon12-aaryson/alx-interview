faf09559529abb9c5f188b18554ce15268399bc2
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-03-14 19:13:02 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Returns a representation of a Pascal's triangle """
+
+
+def pascal_triangle(n):
+    """ Returns a list of lists for Pascal's triangle"""
+    pascal_list = []
+    for row in range(n):
+        if row == 0:
+            pascal_list.append([1])
+        else:
+            previous_row = pascal_list[-1]
+            new_row = [1] + [previous_row[i] + previous_row[i + 1]
+                            for i in range(len(previous_row) - 1)] + [1]
+            pascal_list.append(new_row)
+    return pascal_list
