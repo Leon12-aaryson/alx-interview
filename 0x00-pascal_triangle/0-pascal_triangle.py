934e1e20a4597e4f85c98bb34563a0bd0a1b91f2
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-03-14 19:17:07 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,6 +11,6 @@ def pascal_triangle(n):
         else:
             previous_row = pascal_list[-1]
             new_row = [1] + [previous_row[i] + previous_row[i + 1]
-                            for i in range(len(previous_row) - 1)] + [1]
+                             for i in range(len(previous_row) - 1)] + [1]
             pascal_list.append(new_row)
     return pascal_list
