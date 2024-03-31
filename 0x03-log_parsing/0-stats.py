f59824090cf924c34d0e9a0e9efb7b6078ce1428
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-03-31 20:11:18 +0300

    modification

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,45 +5,43 @@ ALX
 """
 
 import sys
-
+import re
 
 def print_statistics(total_size, status_codes):
-    """ Prints status """
+    """Prints statistics"""
     print(f"File size: {total_size}")
     for code in sorted(status_codes):
         print(f"{code}: {status_codes[code]}")
 
-
 def parse_line(line):
-    """ Parses lines (return) """
-    parts = line.split()
-    if len(parts) >= 7 and parts[5].isdigit():
-        return int(parts[5]), int(parts[6])
-    return None
-
+    """Parses a single line of log data"""
+    pattern = r'(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)'
+    match = re.match(pattern, line)
+    if match:
+        return int(match.group(3)), int(match.group(2))
+    return None, None
 
 def main():
-    """ the main is the start of the program(entry)"""
+    """Main function"""
     total_size = 0
     status_codes = {}
 
     try:
-        for i, line in enumerate(sys.stdin, start=1):
-            data = parse_line(line)
-            if data:
-                file_size, status_code = data
+        line_count = 0
+        for line in sys.stdin:
+            line_count += 1
+            file_size, status_code = parse_line(line)
+            if file_size is not None and status_code is not None:
                 total_size += file_size
-                status_codes[status_code] = \
-                    status_codes.get(status_code, 0) + 1
+                status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
-            if i % 10 == 0:
+            if line_count % 10 == 0:
                 print_statistics(total_size, status_codes)
 
     except KeyboardInterrupt:
-        pass #handle keybARD INTERUPTION
+        pass #keyboard interupt
 
     print_statistics(total_size, status_codes)
-
 
 if __name__ == "__main__":
     main()
