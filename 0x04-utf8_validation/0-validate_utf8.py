6830c8e47889b8d169dce22b0da30b0e2066d76a
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-04 22:46:17 +0300

    modify original code and comment"

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 
 def validUTF8(data):
-    # Helper function to check if a byte is a valid UTF-8 continuation byte
-    def isContinuation(byte):
-        return (byte & 0b11000000) == 0b10000000
+    """
+    Check if a list of integers represents a valid UTF-8 encoding.
 
-    i = 0
-    while i < len(data):
-        num_bytes = 0
-        mask = 0b10000000
-        while data[i] & mask:
-            num_bytes += 1
-            mask >>= 1
+    Args:
+        data (List[int]): List of integers representing bytes.
 
-        if 1 <= num_bytes <= 4:
-            for j in range(1, num_bytes):
-                i += 1
-                if i >= len(data) or not isContinuation(data[i]):
-                    return False
+    Returns:
+        bool: True if data is valid UTF-8, else False.
+    """
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
+
+    # Iterate through each byte in the data
+    for byte in data:
+        # Check if the byte starts a new UTF-8 character
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
+
+            # If the byte is invalid according to UTF-8 rules, return False
+            if num_bytes == 0:
+                continue
+            # If the number of bytes is more than 4 or 1, return False
+            if num_bytes > 4 or num_bytes == 1:
+                return False
         else:
-            return False
+            # If the byte doesn't start with 10, return False
+            if not (byte & (1 << 7) and not (byte & (1 << 6))):
+                return False
+        # Decrement the number of bytes left in the current character
+        num_bytes -= 1
 
-        i += 1
-
-    return True
+    # If all bytes have been processed and num_bytes is 0, return True
+    return num_bytes == 0
