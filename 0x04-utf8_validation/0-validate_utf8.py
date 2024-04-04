07a1d87839c6243186e9b18f0a9961febc4abcba
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-04 22:37:15 +0300

    utf

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def validUTF8(data):
+    # Helper function to check if a byte is a valid UTF-8 continuation byte
+    def isContinuation(byte):
+        return (byte & 0b11000000) == 0b10000000
+
+    i = 0
+    while i < len(data):
+        num_bytes = 0
+        mask = 0b10000000
+        while data[i] & mask:
+            num_bytes += 1
+            mask >>= 1
+
+        if 1 <= num_bytes <= 4:
+            for j in range(1, num_bytes):
+                i += 1
+                if i >= len(data) or not isContinuation(data[i]):
+                    return False
+        else:
+            return False
+
+        i += 1
+
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+## utf validation in python
