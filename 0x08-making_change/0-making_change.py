61cbb263eff5ac6dce4836868378f2c8c0dd4ea0
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-01 12:51:26 +0300

    efficiency of code, reduced number of lines and iterations

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,14 +20,9 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    dp = [0] + [float('inf')] * total
 
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+    return dp[total] if dp[total] != float('inf') else -1
