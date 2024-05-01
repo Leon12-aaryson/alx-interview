7304750b9b720116981fba8f7a3fd3fa2798696d
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-01 12:38:18 +0300

    efficiency check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,19 @@ module that handles changes
 def makeChange(coins, total):
     """
     makechange function
+    :param coins: list of coins
+    :param total: amount to make change for
+    :return: fewest number of coins needed to meet total
+    if total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    coins is a list of the values of the coins in your possession
+    The value of a coin will always be an integer greater than 0
+    You can assume you have an infinite number of each denomination
+    of coin in the list
+    Your solution's runtime will be evaluated in this task
     """
+    if total <= 0:
+        return 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
